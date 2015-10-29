819aaf9c3b51e73796cb6ec33e1402a947f1278e
Mitul Panchal <mpancha@ncsu.edu>
2015-10-29 14:08:09 -0400

    my changes

--- infrastructure.js
+++ infrastructure.js
@@ -3,10 +3,10 @@ var httpProxy = require('http-proxy');
 var exec = require('child_process').exec;
 var request = require("request");
 
-var GREEN = 'http://127.0.0.1:5060';
-var BLUE  = 'http://127.0.0.1:9090';
+var GREEN = 'http://152.46.17.169:5060';
+var BLUE  = 'http://152.46.17.169:9090';
 
-var TARGET = GREEN;
+var TARGET = BLUE;
 
 var infrastructure =
 {
@@ -23,7 +23,7 @@ var infrastructure =
     server.listen(8080);
 
     // Launch green slice
-    exec('forever start deploy/blue-www/main.js 9090', function(err, out, code) 
+    exec('forever -w start deploy/blue-www/main.js 9090', function(err, out, code) 
     {
       console.log("attempting to launch blue slice");
       if (err instanceof Error)
@@ -35,7 +35,7 @@ var infrastructure =
     });
 
     // Launch blue slice
-    exec('forever start deploy/green-www/main.js 5060', function(err, out, code) 
+    exec('forever -w start deploy/green-www/main.js 5060', function(err, out, code) 
     {
       console.log("attempting to launch green slice");
       if (err instanceof Error)
@@ -46,13 +46,22 @@ var infrastructure =
       }
     });
 
-//setTimeout
-//var options = 
-//{
-//  url: "http://localhost:8080",
-//};
-//request(options, function (error, res, body) {
-
+    //setTimeout
+    var heartBeat = setInterval(function(){
+       var options = 
+       {
+          url: "http://localhost:8080",
+       };
+       request(options, function (error, res, body) {
+           console.log(res.statusCode);
+           if ( res.statusCode != 200)
+           {
+              //switch to GREEN
+               TARGET = GREEN;
+               proxy.web( req, res, {target: TARGET } );
+           }
+         })
+       }, 30000)
   },
 
   teardown: function()
